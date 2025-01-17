a29af4777083f7450d3cc3d9c987b45ce9c520bd
Yeisonfjrd <andresfajardo1606@gmail.com>
2025-01-17 17:11:20 -0300

    Initial commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,18 +1,19 @@
-// src/index.js
-const express = require('express');
-const { Pool } = require('pg');
+import express, { Request, Response } from 'express';
+import { Pool } from 'pg';
+
 const app = express();
-const port = process.env.PORT || 3000;  // Puerto predeterminado de Strapi
+const port = process.env.PORT || 3000;
 
 // Conexión a la base de datos PostgreSQL
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/', async (req: Request, res: Response) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM books_to_read;');
-    res.json(rows);
+    app.get('/', (req: Request, res: Response) => {
+      res.json({ message: 'No data to show' }); // Mensaje predeterminado en lugar de los datos
+    });
   } catch (error) {
     console.error('Failed to fetch books', error);
     res.status(500).json({ error: 'Internal Server Error' });
