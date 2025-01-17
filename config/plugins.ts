2d07885aff247e0041bf40ae027bc99dd0fa86be
Yeisonfjrd <andresfajardo1606@gmail.com>
2025-01-17 17:32:29 -0300

    Initial Commi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,25 +1,7 @@
-import express, { Request, Response } from 'express';
-import { Pool } from 'pg';
+import { neon } from "@neondatabase/serverless";
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Conexión a la base de datos PostgreSQL
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-app.use(express.json());
-
-app.get('/', async (req: Request, res: Response) => {
-  try {
-    app.get('/', (req: Request, res: Response) => {
-      res.json({ message: 'No data to show' }); // Mensaje predeterminado en lugar de los datos
-    });
-  } catch (error) {
-    console.error('Failed to fetch books', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running on https://mercadocomic-backend.onrender.com`);
-});
+export async function getData() {
+    const sql = neon(process.env.DATABASE_URL);
+    const data = await sql`SELECT * FROM posts;`;
+    return data;
+}
